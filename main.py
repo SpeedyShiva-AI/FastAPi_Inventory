959bc28202b556cbdd7a8019aba7d98cbd89a9b3
Shivakant-Dubey <study5893@gmail.com>
2026-01-15 16:35:16 +0530

    Add database session dependency

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from models.model import Product
 from models.database import session,Base,engine
 from schemas.product import ProductCreate
+import json
+
+app=FastAPI(title="Inventory and Reorder API")
+
+Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = session()
@@ -10,9 +15,22 @@ def get_db():
     finally:
         db.close()
 
-Base.metadata.create_all(bind=engine)
+def init_db():
+    with open('raw_data.json','r') as f:
+        products=json.load(f)
+    db=session()
+    for item in products:
+        product_data= ProductCreate(**item)
+        db.add(Product(**product_data.model_dump())) 
 
-app=FastAPI(title="Inventory and Reorder API")
+    # db.commit()
+
+# init_db()
+        
+
+@app.get("/products")
+def get_products():
+    return init_db()
 
 @app.get("/")
 def root():
