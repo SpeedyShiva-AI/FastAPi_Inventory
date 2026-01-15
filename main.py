4622aa79d3e113f3a7de0d898af990b628cbe6cf
Shivakant-Dubey <study5893@gmail.com>
2026-01-15 17:53:15 +0530

    Add CRUD API operation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends,HTTPException
 from models.model import Product
 from models.database import session,Base,engine
 from schemas.product import ProductCreate
+from sqlalchemy.orm import Session
 import json
 
 app=FastAPI(title="Inventory and Reorder API")
@@ -15,33 +16,66 @@ def get_db():
     finally:
         db.close()
 
-def init_db():
-    with open('raw_data.json','r') as f:
-        products=json.load(f)
-    db=session()
-    for item in products:
-        product_data= ProductCreate(**item)
-        db.add(Product(**product_data.model_dump())) 
+# def init_db():
+#     with open('raw_data.json','r') as f:
+#         products=json.load(f)
+#     db=session()
+#     for item in products:
+#         product_data= ProductCreate(**item)
+#         db.add(Product(**product_data.model_dump())) 
 
-    # db.commit()
+#     db.commit()
 
 # init_db()
         
-
 @app.get("/products")
-def get_products():
-    return init_db()
+def get_products(db : Session = Depends(get_db)):
+    db_products = db.query(Product).all()
+    return db_products
 
-@app.get("/")
-def root():
-    return{"message":"inventory api is running"}
+@app.post("/products")
+def create_product( product : ProductCreate , db : Session = Depends(get_db) ):
+    db.add(Product(**product.model_dump()))
+    db.commit()
+    return {"message": "Product created successfully"}
 
-@app.get("/health")
-def health_check():
-    return{"status":"ok"}
+
+@app.put("/products/{product_id}")
+def update_product(product_id : int , product: ProductCreate, db : Session = Depends(get_db)):
+    db_product=db.query(Product).filter(Product.id==product_id).first()
+
+    if not db_product:
+        raise HTTPException(status_code=404, detail="product not found")
+    
+    db_product.name=product.name
+    db_product.current_stock = product.current_stock
+    db_product.min_reorder_level = product.min_reorder_level
+    db_product.price = product.price
+
+    db.commit()
+    return {"message": "Product updated successfully"}
+
+
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+
+    if not db_product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    db.delete(db_product)
+    db.commit()
+
+    return {"message": "Product deleted successfully"}
 
 
 # @app.post("/products")
-# def create_products(product: Product):
+# def check():
+#     return{"message":"inventory api is running"}
+
+
+@app.get("/")
+def health_check():
+    return{"status":"ok"}
 
 
